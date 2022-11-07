9968fd8f89ce7fdff24cacf3b0197574fbeef18e
OluchiJoan <ukaigwej19@gmail.com>
2022-11-07 03:19:42 -0500

    Implemented the real life forecast

--- src/index.js
+++ src/index.js
@@ -22,8 +22,55 @@ function formattedDate(real) {
 
   return `${days[day]} ${hour}:${minute}`;
 }
+function formatForecast(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let day = date.getDay();
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thur", "Fri", "Sat"];
+
+  return days[day];
+}
+
+function displayForecast(response) {
+  let forecast = response.data.daily;
+
+  let weatherForecastElement = document.querySelector("#weather-forecast");
+  let weatherForecastHTML = `<div class ="row">`;
+
+  forecast.forEach(function (forecastDay, index) {
+    if (index < 4) {
+      weatherForecastHTML =
+        weatherForecastHTML +
+        `<div class="col-2">
+      <div class="weather-forecast-date"> ${formatForecast(
+        forecastDay.dt
+      )}</div>
+      <img src=" http://openweathermap.org/img/wn/${
+        forecastDay.weather[0].icon
+      }@2x.png" width="40" />
+      <div class="weather-forecast-temp">
+        <span class="weather-forecast-temp-max">
+          <strong>${Math.round(forecastDay.temp.max)}°</strong>
+        </span>
+        <span class="weather-forecast-temp-min">${Math.round(
+          forecastDay.temp.min
+        )}°</span>
+      </div>
+  </div>`;
+    }
+  });
+  weatherForecastHTML = weatherForecastHTML + `</div>`;
+  weatherForecastElement.innerHTML = weatherForecastHTML;
+}
+
 let currentTime = document.querySelector("#real-time");
 currentTime.innerHTML = formattedDate();
+
+function getForecast(coordinates) {
+  console.log(coordinates);
+  let apiKey = "b40b135798f82a05aed08769f9275f50";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}`;
+  axios.get(apiUrl).then(displayForecast);
+}
 
 function showWeather(response) {
   console.log(response.data.weather[0].icon);
@@ -44,7 +91,9 @@ function showWeather(response) {
     "src",
     `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
   );
+  getForecast(response.data.coord);
 }
+
 function searchCity(city) {
   let apiKey = "b40b135798f82a05aed08769f9275f50";
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
@@ -66,7 +115,6 @@ searchForm.addEventListener("submit", findCity);
 function searchLocation(position) {
   let apiKey = "5f472b7acba333cd8a035ea85a0d4d4c";
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&appid=${apiKey}&units=metric`;
-
   axios.get(apiUrl).then(showWeather);
 }
 
@@ -78,4 +126,4 @@ function getCurrentLocation(event) {
 let currentLocationButton = document.querySelector("#current-location-button");
 currentLocationButton.addEventListener("click", getCurrentLocation);
 
-searchCity("Lagos");
+searchCity("Minnesota");
